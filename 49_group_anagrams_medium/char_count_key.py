e367ac1b7fbd59f47f8688d67ec065a085b31bdf
Kaichi-Irie <jjsnoseitodesu@gmail.com>
2025-06-16 18:40:56 +0900

    Refactor anagram key generation method to use tuple for character counts and update README with complexity analysis

--- 49_group_anagrams_medium/char_count_key.py
+++ 49_group_anagrams_medium/char_count_key.py
@@ -12,14 +12,14 @@ from collections import defaultdict
 class Solution:
     def groupAnagrams(self, words: list[str]) -> list[list[str]]:
         # manage anagrams as a hashmap whose key is "canonical key" and value is a list of anagrams
-        anagram_groups: dict[tuple[str], list[str]] = defaultdict(list)
+        anagram_groups: dict[tuple[int, ...], list[str]] = defaultdict(list)
 
         for word in words:
-            canonical_key = self.generate_anagram_key(word)
+            canonical_key = self._generate_anagram_key(word)
             anagram_groups[canonical_key].append(word)
         return list(anagram_groups.values())
 
-    def generate_anagram_key(self, word: str) -> tuple[str]:
+    def _generate_anagram_key(self, word: str) -> tuple[int, ...]:
         char_count = [0] * 26
         for char in word:
             order = ord(char) - ord("a")
