788c2572db3d6fc3275578997b26814e72032d1c
Kaichi-Irie <jjsnoseitodesu@gmail.com>
2025-09-10 09:55:03 +0900

    Add step3

---
+++ 142_linked_list_cycle_ii_medium/step3.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=142 lang=python3
+#
+# [142] Linked List Cycle II
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    NOT_FOUND = -1
+
+    def detectCycle(self, head: ListNode | None) -> ListNode | None:
+        if not head:
+            return None
+        seen_nodes = set()
+        node = head
+        while node:
+            if node in seen_nodes:
+                return node
+            seen_nodes.add(node)
+            node = node.next
+        return None
+
+
+# @lc code=end
