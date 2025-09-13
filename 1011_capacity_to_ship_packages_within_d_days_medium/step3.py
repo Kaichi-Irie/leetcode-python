07f3819a091ecc0f5cd66c09bdb4836d160cbc85
Kaichi-Irie <jjsnoseitodesu@gmail.com>
2025-09-13 13:21:52 +0900

    Add step3 file (no implementations)

---
+++ 1011_capacity_to_ship_packages_within_d_days_medium/step3.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode id=1011 lang=python3
+#
+# [1011] Capacity To Ship Packages Within D Days
+#
+
+# @lc code=start
+class Solution:
+    def shipWithinDays(self, weights: list[int], days: int) -> int:
+        return 0
+# @lc code=end
