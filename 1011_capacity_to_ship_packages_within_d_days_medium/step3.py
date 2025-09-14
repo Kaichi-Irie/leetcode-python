a9921efb4e89cfebe1469954b177bce7652b4ccb
Kaichi-Irie <jjsnoseitodesu@gmail.com>
2025-09-14 14:06:19 +0900

    Add step3

--- 1011_capacity_to_ship_packages_within_d_days_medium/step3.py
+++ 1011_capacity_to_ship_packages_within_d_days_medium/step3.py
@@ -7,5 +7,30 @@
 # @lc code=start
 class Solution:
     def shipWithinDays(self, weights: list[int], days: int) -> int:
-        return 0
+        # for given capacity, we can check if we can ship all the packages with it
+        def can_ship(capacity: int) -> bool:
+            if max(weights) > capacity:
+                return False
+
+            current_weight = 0
+            days_required = 1
+            for weight in weights:
+                current_weight += weight
+                if current_weight > capacity:
+                    current_weight = weight
+                    days_required += 1
+            return days_required <= days
+
+        left = 0
+        right = sum(weights)
+        while right - left > 1:
+            mid = (right + left) // 2
+            if can_ship(mid):
+                right = mid
+            else:
+                left = mid
+
+        return right
+
+
 # @lc code=end
