82c41fa58f2c36b6f16d3029e1c5813544400894
Kaichi-Irie <jjsnoseitodesu@gmail.com>
2025-09-15 16:58:54 +0900

    Add step3 file (no implementations)

---
+++ 63_unique_paths_ii_medium/step3.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=63 lang=python3
+#
+# [63] Unique Paths II
+#
+
+# @lc code=start
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: list[list[int]]) -> int:
+        return 0
+
+
+# @lc code=end
