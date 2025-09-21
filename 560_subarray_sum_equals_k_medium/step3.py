2b74c39fcb092e1f2e0919fa4f998a225c4e57d6
Kaichi-Irie <jjsnoseitodesu@gmail.com>
2025-09-21 19:36:59 +0900

    Add step3 file (no implementations)

---
+++ 560_subarray_sum_equals_k_medium/step3.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=560 lang=python3
+#
+# [560] Subarray Sum Equals K
+#
+
+# @lc code=start
+class Solution:
+    def subarraySum(self, nums: list[int], k: int) -> int:
+        return 0
+
+
+# @lc code=end
