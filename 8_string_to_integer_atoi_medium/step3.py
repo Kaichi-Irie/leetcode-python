15b5b5b857e4967962056ca657987ffc2d4cf684
Kaichi-Irie <jjsnoseitodesu@gmail.com>
2025-09-22 00:01:20 +0900

    Add step3 file (no implementations)

---
+++ 8_string_to_integer_atoi_medium/step3.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=8 lang=python3
+#
+# [8] String to Integer (atoi)
+#
+
+# @lc code=start
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        return 0
+
+
+# @lc code=end
