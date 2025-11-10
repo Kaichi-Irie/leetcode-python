d1f4ef2e009e35c1d240076379ccf73d86a265d2
Kaichi-Irie <jjsnoseitodesu@gmail.com>
2025-11-10 18:32:15 +0900

    Add step3

--- 200_number_of_islands_medium/step3.py
+++ 200_number_of_islands_medium/step3.py
@@ -7,6 +7,32 @@
 # @lc code=start
 class Solution:
     def numIslands(self, grid: list[list[str]]) -> int:
-        return 0
+        if not grid or not grid[0]:
+            return 0
+        WATER = "0"
+        LAND = "1"
+        num_rows = len(grid)
+        num_cols = len(grid[0])
+        num_islands = 0
+        directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
+        def traverse(row, col):
+            for dr, dc in directions:
+                neighbor_row = row + dr
+                neighbor_col = col + dc
+                if neighbor_row < 0 or neighbor_row >= num_rows:
+                    continue
+                if neighbor_col < 0 or neighbor_col >= num_cols:
+                    continue
+                if grid[neighbor_row][neighbor_col] == WATER:
+                    continue
+                grid[neighbor_row][neighbor_col] = WATER
+                traverse(neighbor_row, neighbor_col)
+                
+        for row in range(num_rows):
+            for col in range(num_cols):
+                if grid[row][col] == LAND:
+                    traverse(row, col)
+                    num_islands += 1
+        return num_islands
 
 # @lc code=end
